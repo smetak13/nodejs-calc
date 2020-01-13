2c2a01c0a4bcc1f7328899654b362e1caadcd432
smetak <simon.smejkal@seznam.cz>
2020-01-13 21:50:52 +0100

    added validation

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const readlineSync = require('readline-sync');
 
 const getNumber = order => {
-  const number = readlineSync.question(`Write your ${order} number: `);
+  const number = parseInt(
+    readlineSync.question(`Write your ${order} number: `)
+  );
+  if (isNaN(number)) {
+    console.error(
+      '\x1b[31m',
+      'This is not a number! Please write a real number',
+      '\x1b[0m'
+    );
+    return getNumber(order);
+  }
   return parseInt(number);
 };
 
 const getOperator = () => {
+  const validOperators = ['+', '-', '*', '/'];
   const operator = readlineSync.question('Write your operator: ');
+  if (!validOperators.includes(operator)) {
+    console.error(
+      '\x1b[31m',
+      'Not a valid operator, please use some of the following: +, -, *, /',
+      '\x1b[0m'
+    );
+    return getOperator();
+  }
   return operator;
 };
 
@@ -26,7 +45,7 @@ const calculate = (number1, number2, operator) => {
 };
 
 const writeResult = result => {
-  console.log(`Result is: ${result}`);
+  console.log('\x1b[42m', '\x1b[30m', `Result is: ${result}`, '\x1b[0m');
 };
 
 const number1 = getNumber('first');
